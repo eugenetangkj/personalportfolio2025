583b2c497dc786e719127b0e7f3f25b77b7f4d60
eugenetangkj <dragoneugene10@gmail.com>
2025-05-15 12:38:34 +0800

    Add hero titles

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,15 @@
+
+
+/**
+This component represents the Hero section of the landing page.
+*/
+export default function Hero() {
+    return (
+      <div className='h-screen flex flex-col items-start justify-center'>
+          <h2 className='font-inter text-2xl text-portfolioblack font-medium'>Hello, I&apos;m Eugene Tang!</h2>
+          <h1 className='font-inter text-8xl text-portfolioblack font-extrabold leading-28'>Designing digital products with emphasis on <span className='text-portfoliopurple'>visual</span> design</h1>
+      </div>
+        
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <div>
-        <p className='font-inter text-5xl'>Hello</p>
+    <div className='flex flex-col justify-start items-start mx-72'>
+        <Hero />
     </div>
       
   );
