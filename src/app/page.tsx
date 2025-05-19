6917a22e17eba6a2ed53b9a5a518b0dafd9a3e54
eugenetangkj <dragoneugene10@gmail.com>
2025-05-19 14:27:30 +0800

    Add work experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import CursorFollower from "@/components/common/CursorFollower";
 import Experience from "@/components/Experience";
 import Hero from "@/components/pages/Hero";
 import Projects from "@/components/Projects";
+import WorkExperience from "@/components/pages/WorkExperience";
 
 export default function Home() {
   return (
     <div className='flex flex-col space-y-24 justify-center items-center custom-margin'>
         <Hero />
         <AboutMe />
-        <Hero />
+        <WorkExperience />
         {/* <AboutMe />
         <Experience />
         <Projects /> */}
