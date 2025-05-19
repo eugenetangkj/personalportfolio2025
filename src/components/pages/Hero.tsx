8433e6270fe23322a3febfe504cc0b39eb996817
eugenetangkj <dragoneugene10@gmail.com>
2025-05-19 10:13:36 +0800

    Add animated arrow

--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,6 +1,7 @@
 
 
 import Link from "next/link";
+import { ChevronDown, MoveDown } from "lucide-react";
 
 /**
 This component represents the Hero section of the landing page.
@@ -8,42 +9,49 @@ This component represents the Hero section of the landing page.
 export default function Hero() {
 
     return (
-        <section className="flex flex-col items-center justify-center space-y-12 relative pt-48">
+        <section className="relative flex flex-col h-screen items-center">
 
             {/* Coloured circles */}
-            <div className="bg-[#fcd5ce] animate-sequenced coloured-circle top-[30%] left-[0%] sm:top-[40%] sm:left-[10%]"
+            <div
+                className="bg-[#fcd5ce] animate-sequenced coloured-circle top-[30%] left-[0%] sm:top-[40%] sm:left-[10%]"
                 style={{
-                    transform: 'translate(20%, -50%)',
-                    animationDelay: '2s',
+                transform: 'translate(20%, -50%)',
+                animationDelay: '2s',
                 }}
             />
-            
-            <div className="bg-[#CFE8FA] animate-sequenced coloured-circle top-[60%] left-[20%] sm:top-[70%] sm:left-[35%]"
+            <div
+                className="bg-[#CFE8FA] animate-sequenced coloured-circle top-[40%] left-[30%] sm:top-[50%] sm:left-[45%]"
                 style={{
-                    top: '70%',
-                    left: '35%',
-                    transform: 'translate(20%, -50%)',
-                    animationDelay: '4s',
+                transform: 'translate(20%, -50%)',
+                animationDelay: '4s',
                 }}
             />
 
-            {/* Title */}
-            <h1 className="text-h1-heading text-center">Hi, I am Eugene.</h1>
+            {/* Headers and buttons */}
+            <div className="flex flex-col items-center justify-center space-y-12 flex-1">
+                <h1 className="text-h1-heading text-center">Hi, I am Eugene.</h1>
+                <h2 className="text-h2-heading sm:w-3/4 text-center">
+                Building meaningful, human-centered digital experiences through code and design
+                </h2>
 
-            {/* Subtitle */}
-            <h2 className="text-h2-heading sm:w-3/4 text-center">Building meaningful, human-centered digital experiences through code and design</h2>
-
-            {/* Contact and resume */}
-            <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-8">
+                <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-8">
                 <button className="button-dark">
                     <Link href="#">Let&apos;s Connect</Link>
                 </button>
                 <button className="button-light">
                     <Link href="#">View Resume</Link>
                 </button>
+                </div>
+            </div>
+
+
+            {/* Arrow to navigate */}
+            <div className="mb-6">
+                <Link href='#'>
+                    <ChevronDown className="animate-bounce text-zinc-500 w-8 h-8" />
+                </Link>
             </div>
     </section>
-
     );
   }
   
